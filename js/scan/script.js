fda9c86295bcfc8e277be00020ee13c733c6474c
Lasqutic <91083433+Lasqutic@users.noreply.github.com>
2024-08-08 23:30:05 +0300

    l5

---
+++ js/scan/script.js
@@ -0,0 +1,41 @@
+ document.getElementById('scanButton').addEventListener('click', function() {
+    const scannerContainer = document.getElementById('scanner-container');
+    const scanner = document.getElementById('scanner');
+    const barcodeResult = document.getElementById('barcodeResult');
+
+    // Показать контейнер для сканера
+    scannerContainer.style.display = 'block';
+
+    Quagga.init({
+        inputStream: {
+            name: "Live",
+            type: "LiveStream",
+            target: scanner, // сюда будет выводиться видео
+            constraints: {
+                facingMode: "environment" // использовать заднюю камеру
+            }
+        },
+        decoder: {
+            readers: ["ean_reader"] // Используемый декодер для штрих-кодов EAN
+        }
+    }, function(err) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        console.log("Инициализация завершена. Готов к сканированию");
+        Quagga.start();
+    });
+
+    // Обработка найденного штрих-кода
+    Quagga.onDetected(function(data) {
+        const code = data.codeResult.code;
+        barcodeResult.textContent = code;
+
+        // Остановить сканирование после успешного распознавания
+        Quagga.stop();
+        scannerContainer.style.display = 'none';
+    });
+}); 
+
